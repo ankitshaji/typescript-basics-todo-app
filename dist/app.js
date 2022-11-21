8ab6261baf031290ecbbb49831eba9e20fca9509
Ankit Shaji <ankitshaji@gmail.com>
2022-11-21 14:13:21 +1300

    selecting button element using id

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,3 @@
 "use strict";
-console.log("Connected");
+//elementObject = documentObject.syncWebApiMethod("css id selector")non-null assertion operator
+const btn = document.getElementById("btn");
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,2 @@
-console.log("Connected");
+//elementObject = documentObject.syncWebApiMethod("css id selector")non-null assertion operator
+const btn = document.getElementById("btn")!;
