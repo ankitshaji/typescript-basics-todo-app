1d513843cc8891fb4dc02d6e6634b66048c5321e
Ankit Shaji <ankitshaji@gmail.com>
2022-11-23 23:29:25 +1300

    selecting input element using id

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,4 @@
-//elementObject = documentObject.syncWebApiMethod("css id selector")non-null assertion operator
+//buttonElementObject = documentObject.syncWebApiMethod("css id selector")non-null assertion operator
 const btn = document.getElementById("btn")!;
+//inputElementObject = documentObject.syncWebApiMethod("css id selector")explicit type assertion
+const input = document.getElementById("todoinput") as HTMLInputElement;
