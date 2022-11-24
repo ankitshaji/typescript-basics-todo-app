f851c465baf4c8f2fdf9fc74d169aa9d43bad21f
Ankit Shaji <ankitshaji@gmail.com>
2022-11-24 17:55:00 +1300

    add click event listener on buttonElemObj to alert + clear input's value

--- src/app.ts
+++ src/app.ts
@@ -2,3 +2,8 @@
 const btn = document.getElementById("btn")!;
 //inputElementObject = documentObject.syncWebApiMethod("css id selector")explicit type assertion
 const input = document.getElementById("todoinput") as HTMLInputElement;
+//buttonElementObject.asyncWebApiMethod("clickEventString",normalAnonymousFunctionExpressionDefinitionCallback(parameter-clickEventObject))
+btn.addEventListener("click", function (e) {
+  window.alert(input.value);
+  input.value = "";
+});
