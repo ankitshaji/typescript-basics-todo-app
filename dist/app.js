43f2dbbea9cf9af6daae824fa2f4c7dad389ca15
Ankit Shaji <ankitshaji@gmail.com>
2022-11-26 02:40:20 +1300

    code seperation+create then push newTodoObject into arrayObject instance

--- dist/app.js
+++ dist/app.js
@@ -9,26 +9,36 @@ const input = document.getElementById("todoinput");
 const form = document.querySelector("form");
 //ulElementObject:HTMLElement = documentObject.syncWebApiMethod("css element selector")
 const ul = document.getElementById("todolist");
-//non-inline namedFunctionExpressionDefenitionCallback(parameter that accepts argument submitEventObject)
-function handleSubmitCallback(e) {
-    //submitEventObject.syncWebApimethod()
-    e.preventDefault();
-    //inputElementObject.property
-    const newTodoText = input.value;
+//namedFunctionExpressionDefenition(parameter that accepts argument todoObject)
+function createElements(myParTodoObject) {
     //liElementObject:HTMLLIElement = documentObject.syncWebApiMethod("elementName")
     const newLi = document.createElement("li");
     //inputElementObject:HTMLInputElement = documentObject.syncWebApiMethod("elementName")
     const newCheckbox = document.createElement("input");
     //inputElementObject.property
     newCheckbox.type = "checkbox";
-    //liElementObject.syncMethod("stringObject")
-    newLi.append(newTodoText);
+    //liElementObject.syncMethod(myParTodoObject.property = "stringObject")
+    newLi.append(myParTodoObject.text);
     //liElementObject.syncMethod(elementObject)
     newLi.append(newCheckbox);
     //ulElementObject explicit optional operator.syncMethod(elementObject)
     ul === null || ul === void 0 ? void 0 : ul.append(newLi);
     //inputElementObject.property
     input.value = "";
+}
+//non-inline namedFunctionExpressionDefenitionCallback(parameter that accepts argument submitEventObject)
+function handleSubmitCallback(e) {
+    //submitEventObject.syncWebApimethod()
+    e.preventDefault();
+    //declaring a variable that stores reference to objectLiteral(todoObject)
+    const newTodoObject = {
+        //inputElementObject.property
+        text: input.value,
+        completed: false,
+    };
+    //namedFunctionExpressionDefenition(todoObject)
+    createElements(newTodoObject);
+    myVarTodoObjectArrayObject.push(newTodoObject);
 }
 //formElementObject.asyncWebApiMethod("submitEventString",non-inline namedFunctionExpressionDefenitionCallback(parameter that accepts argument submitEventObject))
 form.addEventListener("submit", handleSubmitCallback);
--- src/app.ts
+++ src/app.ts
@@ -15,26 +15,36 @@ const form = document.querySelector("form")!;
 //ulElementObject:HTMLElement = documentObject.syncWebApiMethod("css element selector")
 const ul = document.getElementById("todolist");
 
-//non-inline namedFunctionExpressionDefenitionCallback(parameter that accepts argument submitEventObject)
-function handleSubmitCallback(e: SubmitEvent): void {
-  //submitEventObject.syncWebApimethod()
-  e.preventDefault();
-  //inputElementObject.property
-  const newTodoText = input.value;
+//namedFunctionExpressionDefenition(parameter that accepts argument todoObject)
+function createElements(myParTodoObject: TodoObjectInterface): void {
   //liElementObject:HTMLLIElement = documentObject.syncWebApiMethod("elementName")
   const newLi = document.createElement("li");
   //inputElementObject:HTMLInputElement = documentObject.syncWebApiMethod("elementName")
   const newCheckbox = document.createElement("input");
   //inputElementObject.property
   newCheckbox.type = "checkbox";
-  //liElementObject.syncMethod("stringObject")
-  newLi.append(newTodoText);
+  //liElementObject.syncMethod(myParTodoObject.property = "stringObject")
+  newLi.append(myParTodoObject.text);
   //liElementObject.syncMethod(elementObject)
   newLi.append(newCheckbox);
   //ulElementObject explicit optional operator.syncMethod(elementObject)
   ul?.append(newLi);
   //inputElementObject.property
   input.value = "";
+}
+//non-inline namedFunctionExpressionDefenitionCallback(parameter that accepts argument submitEventObject)
+function handleSubmitCallback(e: SubmitEvent): void {
+  //submitEventObject.syncWebApimethod()
+  e.preventDefault();
+  //declaring a variable that stores reference to objectLiteral(todoObject)
+  const newTodoObject: TodoObjectInterface = {
+    //inputElementObject.property
+    text: input.value,
+    completed: false,
+  };
+  //namedFunctionExpressionDefenition(todoObject)
+  createElements(newTodoObject);
+  myVarTodoObjectArrayObject.push(newTodoObject);
 }
 
 //formElementObject.asyncWebApiMethod("submitEventString",non-inline namedFunctionExpressionDefenitionCallback(parameter that accepts argument submitEventObject))
