0ea0d06de33447748f4c29ab4c62784bc0e0d8d9
Ankit Shaji <ankitshaji@gmail.com>
2022-11-25 20:36:57 +1300

    selecting ul element using id

--- dist/app.js
+++ dist/app.js
@@ -1,13 +1,14 @@
 "use strict";
-//buttonElementObject = documentObject.syncWebApiMethod("css id selector")non-null assertion operator
+//buttonElementObject:HTMLElement = documentObject.syncWebApiMethod("css id selector")non-null assertion operator
 const btn = document.getElementById("btn");
-//inputElementObject = documentObject.syncWebApiMethod("css id selector")explicit type assertion
+//inputElementObject:HTMLInputElement = documentObject.syncWebApiMethod("css id selector")explicit type assertion
 const input = document.getElementById("todoinput");
-//formElementObject = documentObject.syncWebApiMethod("css element selector")non null assertion
+//formElementObject:HTMLFormElement = documentObject.syncWebApiMethod("css element selector")non null assertion
 const form = document.querySelector("form");
+//ulElementObject:HTMLElement = documentObject.syncWebApiMethod("css element selector")
+const ul = document.getElementById("todolist");
 //formElementObject.asyncWebApiMethod("submitEventString",inline normalAnonymousFunctionExpressionDefinitionCallback(parameter that accepts argument submitEventObject))
 form.addEventListener("submit", function (e) {
     //submitEventObject.syncWebApimethod()
     e.preventDefault();
-    console.log("Submitted");
 });
